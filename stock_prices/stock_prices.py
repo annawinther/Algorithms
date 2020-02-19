2ca43a0c718cdbfa3e8c8c327ef28bfea35012e4
Anna Winther <anski.anna@gmail.com>
2020-02-19 13:34:37 +0000

    Psudo code for stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,6 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
+  # 9 lines of code?
+  # Find the maximum difference between smallest and largest price in list
+  # current_min_price_so_far and the max_profit_so_far?
+  # min price has to come before --> make a loop
+  # assign first value to min price so far...
+  # - min price --> [0] in array
+  # max price (currently on) - min price
+  # max profit 
+  # loop: if you find price smaller than current price - set that to be smallest - else continue with that mic price
+  # curr profit just calculates  --> currr profit net i in the list minus the min - price
+  # if price at index we're at is lower than min price - set it to be the min price
   pass
 
 
