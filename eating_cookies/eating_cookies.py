b7e7b1efecdff9cf48d908af5cf45d1a263e6b51
Anna Winther <anski.anna@gmail.com>
2020-02-19 16:27:25 +0000

    Started eating_cookies, just psudo code so far

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,6 +6,24 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+  # less or equal to 1 return 1 --> base case
+  if n <= 1:
+    return 1
+  # if we give 5:
+    # 1 1 1 1 1
+    # 1 1 1 2
+    # 1 1 2 1
+    # 1 1 3
+    # 1 2 1 1
+    # 1 2 2
+    # 2 1 1 1
+    # 2 1 2
+    # 2 2 1
+    # 2 3
+    # 3 1 1
+    # 3 2
+
+
   pass
 
 if __name__ == "__main__":
