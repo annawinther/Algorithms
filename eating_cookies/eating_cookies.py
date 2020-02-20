562248d347cab6a17adbfba80b1b4b892c41b94a
Anna Winther <anski.anna@gmail.com>
2020-02-20 13:08:28 +0000

    Add code to eating_cookies using the cache - test passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -21,18 +21,28 @@ def eating_cookies_without_cache(n, cache=None):
 
 def eating_cookies(n, cache=None):
   # if the n is less than zero
+  if n < 0:
     # return zero
+    return 0
   # otherwise if n is equal to zero
+  elif n == 0: 
     # return one
-  # the case of the cache at index of n contains the answer
-    # return the cache at index of n 
+    return 1
+  # otherwise if the case of the cache at index of n contains the answer
+  elif cache and cache[n] > 0:
+    # return the cache at index of n
+    return cache[n] 
   # otherwise
+  else:
     # test if there is a cache at all (if not)
+    if not cache:
       # create a cache using a range based loop
+      cache = {i: 0 for i in range(n + 1)}
     
     # set the cache at index n to the recursive call passing th ecache as we go
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
     # return the cache at index n
-    pass
+    return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
