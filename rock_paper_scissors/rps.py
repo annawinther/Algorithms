7fc7abf7361c4e9146e6fcdfcd51f26ec7a97cb8
Anna Winther <anski.anna@gmail.com>
2020-02-20 14:18:29 +0000

    Psuedo code finished for rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -10,21 +10,27 @@ n is the rounds of the game
 
 if n = 1 --> im playing 1 round and all the possible outcomes is: rock_paper_scissors(1), [['rock'], ['paper'], ['scissors']]
 
-I'll be doing a for loop inside of a for loop becasue I want to create ALL of the possible moves in sequential order based on the number of roiunds being played (or passed in to the function)
+I'll be doing a for loop inside of a for loop becasue I want to create ALL of the possible moves in sequential order based on the number of rounds being played (or passed in to the function)
 """
 
 import sys
 
 def rock_paper_scissors(n):
   # set the different moves to be a list off all different moves
-  
-  # set the list to be an empty list 
 
-  # if n is equal to 0 
-    # return empty list
-  # otherwise if n is 1 
-    # return list.append['rock'], ['paper'], ['scissors']
-  # 
+  # set the outcomes to be an empty list 
+
+  # define a helperfuncition that takes in the rounds remaining and the result (initialize as an empty list)
+    # have a base case is there are no more rounds left 
+      # return the outcomes and append the result from the function
+    # do a for in loop in the moves lists
+      # this is a recursive call
+      # using the helperfunction decrementing the remaining rounds for each itteration and adding the resutl together with reach move 
+      
+  # then invoking the helperfunction
+
+  # and return the outcomes form that
+
   pass 
 
 
